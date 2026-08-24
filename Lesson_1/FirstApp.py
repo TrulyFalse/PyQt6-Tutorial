e474495a337809e59868e74084d97dba4068477a
Abrar Ur Alam <safee20142@gmail.com>
2026-08-25 00:48:24 +0600

    Create QMainWindow-ed app with QPushButton

--- Lesson_1/FirstApp.py
+++ Lesson_1/FirstApp.py
@@ -1,9 +1,19 @@
-from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
 
 import sys
 
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("My App")
+        button = QPushButton("Press Me!")
+
+        self.setCentralWidget(button)
+
+
 app = QApplication(sys.argv)
-window = QWidget()
+window = MainWindow()
 window.show()
 
 app.exec()
