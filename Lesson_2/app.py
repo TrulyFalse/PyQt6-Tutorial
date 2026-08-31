d88ea503c0b83cf182c948cc392625a94c5b1b62
Abrar Ur Alam <safee20142@gmail.com>
2026-08-31 19:50:44 +0600

    Access state directly from widget rather than signal in handler

--- Lesson_2/app.py
+++ Lesson_2/app.py
@@ -5,21 +5,20 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
+        self.button_is_checked = True
         self.setWindowTitle("My App")
 
-        button = QPushButton("Press Me!")
-        button.setCheckable(True)
-        button.clicked.connect(self.the_button_was_clicked)
-        button.clicked.connect(self.the_button_was_toggled)
+        self.button = QPushButton("Press Me!")
+        self.button.setCheckable(True)
+        self.button.released.connect(self.the_button_was_released)
+        self.button.setChecked(self.button_is_checked)
 
         # Set the central wdget of the window
-        self.setCentralWidget(button)
+        self.setCentralWidget(self.button)
 
-    def the_button_was_clicked(self):
-        print("Clicked!")
-
-    def the_button_was_toggled(self, isChecked):
-        print('Checked?', isChecked)
+    def the_button_was_released(self):
+        self.button_is_checked = self.button.isChecked()
+        print(self.button_is_checked)
 
 app = QApplication(sys.argv)
 window = MainWindow()
