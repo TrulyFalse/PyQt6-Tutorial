bef5b8ea09a32494d4425fdd623448c83fe29fef
Abrar Ur Alam <safee20142@gmail.com>
2026-09-04 19:41:56 +0600

    Nested Layout

--- Lesson_4/app.py
+++ Lesson_4/app.py
@@ -1,6 +1,6 @@
 import sys
 
-from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QHBoxLayout
+from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QHBoxLayout, QVBoxLayout
 from layout_colorwidget import Color
 
 
@@ -9,15 +9,28 @@ class MainWindow(QMainWindow):
         super().__init__()
         self.setWindowTitle("My App")
 
-        layout = QHBoxLayout()
+        layout1 = QHBoxLayout()
+        layout2 = QVBoxLayout()
+        layout3 = QVBoxLayout()
 
-        layout.addWidget(Color("red"))
-        layout.addWidget(Color("green"))
-        layout.addWidget(Color("orange"))
-        layout.addWidget(Color("blue"))
+        layout2.addWidget(Color("red"))
+        layout2.addWidget(Color("yellow"))
+        layout2.addWidget(Color("purple"))
+
+        layout1.setContentsMargins(0,0,0,0)
+        layout1.setSpacing(1)
+
+        layout1.addLayout(layout2)
+
+        layout1.addWidget(Color("green"))
+
+        layout3.addWidget(Color("red"))
+        layout3.addWidget(Color("purple"))
+
+        layout1.addLayout(layout3)
 
         widget = QWidget()
-        widget.setLayout(layout)
+        widget.setLayout(layout1)
         self.setCentralWidget(widget)
 
 
